5b300cb78e6c9ba6b14df4b32075e578417e5e14
Amie Wei <amie.wei@hashicorp.com>
2023-10-30 09:09:28 -0400

    update actions and configure vault to use globs

--- oidc-github.sh
+++ oidc-github.sh
@@ -2,43 +2,35 @@
 # https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-hashicorp-vault
 
 
-# # Configure a policy named github-actions that only grants access to the specific paths
-# vault policy write github-actions - <<EOF
-# # Read-only permission on 'secret/data/github-actions/*' path
+# Configure a policy named github-actions that only grants access to the specific paths
+vault policy write github-actions - <<EOF
+# Read-only permission on 'secret/data/ci/*' path
 
-# path "secret/data/github-actions/*" {
-#   capabilities = [ "read" ]
-# }
-# EOF
+path "secret/data/ci/*" {
+  capabilities = [ "read" ]
+}
+EOF
 
-# vault policy write github-actions - <<EOF
-# # Read-only permission on 'secret/data/github-actions/my-secret/*' path
+# Configure roles to group different policies together. 
+# If the authentication is successful, these policies are attached to the resulting Vault access token
+# if planning to use globs (*),  need to add in additional "bound_claims_type": "glob"
+vault write auth/jwt/role/github-actions -<<EOF
+{
+  "role_type": "jwt",
+  "user_claim": "actor",
+  "bound_claims_type": "glob",
+  "bound_claims": {
+    "repository": "amie-org/*"
+  },
+  "policies": ["github-actions"],
+  "ttl": "15m"
+}
+EOF
 
-# path "secret/data/github-actions/my-secret/*" {
-#   capabilities = [ "read" ]
-# }
-# EOF
+# write a secret named my-secret at the kv v2 path
+vault kv put secret/ci/github-actions hello=action 
 
-# # Configure roles to group different policies together. 
-# # If the authentication is successful, these policies are attached to the resulting Vault access token.auth/jwt/role/myproject-production -<<EOF
-# vault write auth/jwt/role/github-actions -<<EOF
-# {
-#   "role_type": "jwt",
-#   "user_claim": "actor",
-#   "bound_claims": {
-#     "repository": "amie-org/*"
-#   },
-#   "policies": ["github-actions"],
-#   "ttl": "15m"
-# }
-# EOF
-
-# # write a secret named my-secret at the kv v2 path
-# vault kv put secret/github-actions/my-secret hello=action 
-
-# vault kv put secret/github-actions hello=action
-
-
+########################################################################
 ###### github provided example #######
 vault write auth/jwt/config \
   bound_issuer="https://token.actions.githubusercontent.com" \
@@ -52,17 +44,8 @@ path "secret/data/production/*" {
 }
 EOF
 
-# vault write auth/jwt/role/myproject-production -<<EOF
-# {
-#   "role_type": "jwt",
-#   "user_claim": "actor",
-#   "bound_claims": {
-#     "repository": "amie-org/vault-github-oidc-auth"
-#   },
-#   "policies": ["myproject-production"],
-#   "ttl": "10m"
-# }
 
+# if planning to use globs (*),  need to add in additional "bound_claims_type": "glob"
 vault write auth/jwt/role/myproject-production -<<EOF
 {
   "role_type": "jwt",
